917bd3d43c4cac71366dc473c1e2c927112bedec
Vincent Zhu <vincentzhu@ucsb.edu>
2022-09-19 21:35:56 -0700

    P33 O(log n) Solution

--- Medium/P33_Search_in_Rotated_Sorted_Array.py
+++ Medium/P33_Search_in_Rotated_Sorted_Array.py
@@ -16,22 +16,27 @@ def search(nums, target):
     :type target: int
     :rtype: int
     """
-    res = -1
     l, r = 0, len(nums) - 1
     
     while l <= r:
-        m = (l + r)/2
+        m = (l + r)//2
         if nums[m] == target:
-            res = m
-        elif nums[m] > target:
-            l = m + 1
-        elif nums[m] < target:
-            r = m - 1
-    return res   
+            return m
+        if nums[l] <= nums[m]:
+            if target > nums[m] or target < nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        else:
+            if target < nums[m] or target > nums[r]:
+                r = m - 1
+            else:
+                l = m + 1
+    return -1   
         
 def main():
-    nums = [4,5,6,7,0,1,2]
-    target = 0
+    nums = [4,5,6,7,8,1,2,3]
+    target = 8
     print(search(nums, target))
 
 if __name__ == '__main__':
